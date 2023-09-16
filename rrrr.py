ed8b289e01c8765e52d642aee31d6566a9b33530
woosang <dnehtks123@naver.com>
2023-09-16 19:43:01 +0900

    CT 4-1

--- rrrr.py
+++ rrrr.py
@@ -1,17 +1,49 @@
-left = list(input())
-N = len(left)
-right =[]
-M = int(input())
+# 이코테 상하좌우 문제 풀이 따라써보기
 
-for i in M:
-    command = list(sys.stdin.readline().split())
-    if command[0] == 'L' and left:
-        left.pop()
-    elif command[0] =='D' and right:
-        left.append(right.pop())
-    elif command[0] == 'B' and left:
-        left.pop()
-    elif command[0] == 'P' and left:
-        left.append(command[1])
-        
-print(''.join(left + right[-1]))
+n = int(input)
+plans = sys.stdin.split.rstrip()
+x, y = 1, 1
+
+
+# 이동
+dx = [0, 0, -1, 1]
+dy = [-1, 1, 0, 0]
+
+move_types=[L, R, U, D]
+
+
+# 이동계획을 하나씩 확인
+for i in plans:
+    # 이동 후 좌표 구하기
+    for plan in range(len(move_types)):
+        if plan == move_types[i]:
+            nx = x + dx[i]
+            ny = y + dy[i]
+#공간을 벗어나는 경우 무시
+if nx < 1  or ny < 1 or nx > n or ny > n:
+    continue
+# 이동 수행
+
+x, y = nx, ny
+
+
+
+
+# 직접 생각하면서 따라서 작성 
+n = int(input())
+plans = int(input().split())
+
+x,y=1,1
+
+move_types=['L', 'R', 'U', 'U']
+
+dx = [0, 0, -1, 1]
+dy = [-1, 1, 0, 0]
+
+for i in plans:
+    for plan in range(len(move_types)):
+        if plan == move_types:
+            nx = x + dx[i]
+            ny = y + dy[i]
+
+if nx > n or ny > n or nx < 1 or ny < 1 
